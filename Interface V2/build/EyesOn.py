e3515fc926299bc054eb6d65be603cafc14b2f19
Matheus Maciel <maathdesigner@outlook.com>
2024-08-22 08:22:18 -0300

    Interface tela cheia

--- Interface V2/build/EyesOn.py
+++ Interface V2/build/EyesOn.py
@@ -4,7 +4,7 @@ import webbrowser, time, pyautogui
 
 window = Tk()
 
-window.geometry("340x480")
+window.geometry("1920x1080")
 window.configure(bg = "#FFFFFF")                        
 
 def main():
@@ -12,27 +12,27 @@ def main():
     global button_image_1, button_image_2, button_image_3, button_image_4
 
     OUTPUT_PATH = Path(__file__).parent
-    ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\maath\OneDrive - Instituto Nacional de Telecomunicações\EyesOn Fetin\TkDesigner Interface\build\assets\frame0")
+    ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\maath\Desktop\EyesOn\Interface V2\build\assets\frame0")
 
     def relative_to_assets(path: str) -> Path:
         return ASSETS_PATH / Path(path)
 
     canvas = Canvas(
-        window,
-        bg = "#FFFFFF",
-        height = 480,
-        width = 340,
-        bd = 0,
-        highlightthickness=0,
-        relief = "ridge"
+    window,
+    bg = "#FFFFFF",
+    height = 1080,
+    width = 1920,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
     )
 
     canvas.place(x = 0, y = 0)
     image_image_1 = PhotoImage(
         file=relative_to_assets("image_1.png"))
     image_1 = canvas.create_image(
-        170.0,
-        240.0,
+        960.0,
+        540.0,
         image=image_image_1
     )
 
@@ -46,10 +46,10 @@ def main():
         relief="flat"
     )
     button_1.place(
-        x=197.0,
-        y=156.0,
-        width=104.0,
-        height=69.0
+        x=1426.0,
+        y=216.0,
+        width=319.0,
+        height=204.0
     )
 
     button_image_2 = PhotoImage(
@@ -62,10 +62,10 @@ def main():
         relief="flat"
     )
     button_2.place(
-        x=41.0,
-        y=156.0,
-        width=104.0,
-        height=69.0
+        x=960.0,
+        y=216.0,
+        width=305.0,
+        height=204.0
     )
 
     button_image_3 = PhotoImage(
@@ -78,10 +78,10 @@ def main():
         relief="flat"
     )
     button_3.place(
-        x=41.0,
-        y=260.0,
-        width=104.0,
-        height=69.0
+        x=960.0,
+        y=540.0,
+        width=305.0,
+        height=192.0
     )
 
     button_image_4 = PhotoImage(
@@ -90,21 +90,21 @@ def main():
         image=button_image_4,
         borderwidth=0,
         highlightthickness=0,
-        command=lambda: print("button_4 clicked"),
+        command=lambda: print("Zona Morta"),
         relief="flat"
     )
     button_4.place(
-        x=200.0,
-        y=260.0,
-        width=104.0,
-        height=69.0
+        x=1426.0,
+        y=540.0,
+        width=319.0,
+        height=192.0
     )
 def screen1():
     global image_image_1  # Torna a referência global
     global button_image_1, button_image_2, button_image_3, button_image_4, button_image_5, button_image_6
 
     OUTPUT_PATH = Path(__file__).parent
-    ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\maath\OneDrive - Instituto Nacional de Telecomunicações\EyesOn Fetin\TkDesigner Interface\build\assets\frame1")
+    ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\maath\Desktop\EyesOn\Interface V2\build\assets\frame1")
 
     def relative_to_assets(path: str) -> Path:
         return ASSETS_PATH / Path(path)
@@ -112,8 +112,8 @@ def screen1():
     canvas = Canvas(
     window,
     bg = "#FFFFFF",
-    height = 480,
-    width = 340,
+    height = 1080,
+    width = 1920,
     bd = 0,
     highlightthickness = 0,
     relief = "ridge"
@@ -123,29 +123,29 @@ def screen1():
     image_image_1 = PhotoImage(
         file=relative_to_assets("image_1.png"))
     image_1 = canvas.create_image(
-        170.0,
-        240.0,
+        960.0,
+        540.0,
         image=image_image_1
     )
-
     #Abrindo guia de pesquisa
     def openurl():
         url = f"https://www.google.com"
         webbrowser.open(url)
+
     button_image_1 = PhotoImage(
         file=relative_to_assets("button_1.png"))
     button_1 = Button(
         image=button_image_1,
         borderwidth=0,
         highlightthickness=0,
-        command=openurl,
+        command=main,
         relief="flat"
     )
     button_1.place(
-        x=40.0,
-        y=105.0,
-        width=104.0,
-        height=69.0
+        x=1426.0,
+        y=131.0,
+        width=317.0,
+        height=212.0
     )
 
     button_image_2 = PhotoImage(
@@ -154,20 +154,20 @@ def screen1():
         image=button_image_2,
         borderwidth=0,
         highlightthickness=0,
-        command=main,
+        command=openurl,
         relief="flat"
     )
     button_2.place(
-        x=196.0,
-        y=105.0,
-        width=104.0,
-        height=69.0
+        x=945.0,
+        y=131.0,
+        width=311.0,
+        height=212.0
     )
 
-    #Botão responsável por abrir mais uma guia da internet
-    def opennewtab():
-        url = f"https://www.google.com"
-        webbrowser.open_new_tab(url)
+    #Abrindo youtube
+    def openyoutube():
+        url = f"https://www.youtube.com/"
+        webbrowser.open(url)
 
     button_image_3 = PhotoImage(
         file=relative_to_assets("button_3.png"))
@@ -175,44 +175,46 @@ def screen1():
         image=button_image_3,
         borderwidth=0,
         highlightthickness=0,
-        command=opennewtab,
+        command=openyoutube,
         relief="flat"
     )
     button_3.place(
-        x=39.0,
-        y=208.0,
-        width=104.0,
-        height=69.0
+        x=945.0,
+        y=455.0,
+        width=311.0,
+        height=210.0
     )
 
+    #Abrir netflix
+    def opennetflix():
+        url = f"https://www.netflix.com/br/"
+        webbrowser.open(url)
 
-    #Botão responsável por fechar a guia da internet atual
-    def closetab():
-        time.sleep(2)
-        pyautogui.hotkey('alt', 'tab')
-        time.sleep(2)
-        pyautogui.hotkey('ctrl', 'w')
-    
     button_image_4 = PhotoImage(
         file=relative_to_assets("button_4.png"))
     button_4 = Button(
         image=button_image_4,
         borderwidth=0,
         highlightthickness=0,
-        command=closetab,
+        command=opennetflix,
         relief="flat"
     )
     button_4.place(
-        x=196.0,
-        y=208.0,
-        width=104.0,
-        height=69.0
+        x=1426.0,
+        y=455.0,
+        width=328.0,
+        height=210.0
     )
+    def closetabs_():
+      time.sleep(2)
+      pyautogui.hotkey('alt', 'tab')
+      time.sleep(2)
+      pyautogui.hotkey('alt', 'f4')
 
-    #Botão que alterna as guias da internet
-    def switchtabs():
-        time.sleep(2)
-        pyautogui.hotkey('ctrl', 'tab')
+    #Abrir netflix
+    def opentwitter():
+        url = f"https://x.com/"
+        webbrowser.open(url)
 
     button_image_5 = PhotoImage(
         file=relative_to_assets("button_5.png"))
@@ -220,35 +222,30 @@ def screen1():
         image=button_image_5,
         borderwidth=0,
         highlightthickness=0,
-        command=switchtabs,
+        command=closetabs_,
         relief="flat"
     )
     button_5.place(
-        x=39.0,
-        y=312.0,
-        width=104.0,
-        height=69.0
+        x=1421.0,
+        y=778.0,
+        width=322.0,
+        height=215.0
     )
-
-    def closetabs_():
-
-        time.sleep(2)
-        pyautogui.hotkey('alt', 'ft')
-
+   
     button_image_6 = PhotoImage(
         file=relative_to_assets("button_6.png"))
     button_6 = Button(
         image=button_image_6,
         borderwidth=0,
         highlightthickness=0,
-        command=closetabs_,
+        command=opentwitter,
         relief="flat"
     )
     button_6.place(
-        x=196.0,
-        y=312.0,
-        width=104.0,
-        height=69.0
+        x=945.0,
+        y=778.0,
+        width=311.0,
+        height=215.0
     )
 
 def screen2():
@@ -256,27 +253,27 @@ def screen2():
     global button_image_1, button_image_2, button_image_3, button_image_4, button_image_5, button_image_6, button_image_7
 
     OUTPUT_PATH = Path(__file__).parent
-    ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\maath\OneDrive - Instituto Nacional de Telecomunicações\EyesOn Fetin\TkDesigner Interface\build\assets\frame2")
+    ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\maath\Desktop\EyesOn\Interface V2\build\assets\frame2")
 
     def relative_to_assets(path: str) -> Path:
         return ASSETS_PATH / Path(path)
 
     canvas = Canvas(
-        window,
-        bg = "#FFFFFF",
-        height = 480,
-        width = 340,
-        bd = 0,
-        highlightthickness = 0,
-        relief = "ridge"
+    window,
+    bg = "#FFFFFF",
+    height = 1080,
+    width = 1920,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
     )
 
     canvas.place(x = 0, y = 0)
     image_image_1 = PhotoImage(
         file=relative_to_assets("image_1.png"))
     image_1 = canvas.create_image(
-        170.0,
-        240.0,
+        960.0,
+        540.0,
         image=image_image_1
     )
 
@@ -290,10 +287,10 @@ def screen2():
         relief="flat"
     )
     button_1.place(
-        x=40.0,
-        y=34.0,
-        width=104.0,
-        height=69.0
+        x=1424.0,
+        y=134.0,
+        width=316.0,
+        height=208.0
     )
 
     button_image_2 = PhotoImage(
@@ -306,10 +303,10 @@ def screen2():
         relief="flat"
     )
     button_2.place(
-        x=196.0,
-        y=34.0,
-        width=104.0,
-        height=69.0
+        x=936.0,
+        y=134.0,
+        width=326.0,
+        height=208.0
     )
 
     button_image_3 = PhotoImage(
@@ -322,12 +319,11 @@ def screen2():
         relief="flat"
     )
     button_3.place(
-        x=40.0,
-        y=138.0,
-        width=104.0,
-        height=69.0
+        x=936.0,
+        y=458.0,
+        width=326.0,
+        height=194.0
     )
-
     #Abrir explorador de arquivos
     def explorer():
         time.sleep(2)
@@ -343,10 +339,10 @@ def screen2():
         relief="flat"
     )
     button_4.place(
-        x=196.0,
-        y=138.0,
-        width=104.0,
-        height=69.0
+        x=1424.0,
+        y=458.0,
+        width=326.0,
+        height=194.0
     )
 
     button_image_5 = PhotoImage(
@@ -359,10 +355,10 @@ def screen2():
         relief="flat"
     )
     button_5.place(
-        x=40.0,
-        y=242.0,
-        width=104.0,
-        height=69.0
+        x=1424.0,
+        y=779.0,
+        width=326.0,
+        height=205.0
     )
 
     button_image_6 = PhotoImage(
@@ -375,26 +371,10 @@ def screen2():
         relief="flat"
     )
     button_6.place(
-        x=196.0,
-        y=243.0,
-        width=104.0,
-        height=69.0
-    )
-
-    button_image_7 = PhotoImage(
-        file=relative_to_assets("button_7.png"))
-    button_7 = Button(
-        image=button_image_7,
-        borderwidth=0,
-        highlightthickness=0,
-        command=lambda: print("button_7 clicked"),
-        relief="flat"
-    )
-    button_7.place(
-        x=196.0,
-        y=349.0,
-        width=104.0,
-        height=69.0
+        x=936.0,
+        y=779.0,
+        width=326.0,
+        height=205.0
     )
     
 window.resizable(False, False)
